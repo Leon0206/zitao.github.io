68feba557e39ef5f7f3032be066d7e156677ab6b
lizitao <634376133@qq.com>
2024-02-27 00:11:21 +0100

    update

--- killPortFor4000.sh
+++ killPortFor4000.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-# 获取位置
+# Get location
 
 lsof -i :4000
 
 echo ""
 
-echo "Please execute kill -9  [PID]"
+echo "Get all processes containing "node", extract the PID and kill them all:"
+
+PIDS=$(lsof -i :terabase | grep "node" | awk '{print $2}')
+
+# Traverse all found PIDs and kill the corresponding process
+for PID in $PIDS; do
+     echo "Killing process with PID: $PID"
+     kill -9 $PID
+done
